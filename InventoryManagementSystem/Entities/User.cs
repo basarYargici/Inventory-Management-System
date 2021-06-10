25a5f45b7a72ebf76d3cac803cc4aa88da9be4f6
basarYargici <ibrahimbyargici@gmail.com>
2021-06-10 18:37:29 +0300

    User validation, sign up and login features added.

---
+++ InventoryManagementSystem/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿namespace InventoryManagementSystem.Entities
+{
+    class User
+    {
+        private string name;
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public User(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
--- InventoryManagementSystem/Form1.cs
+++ InventoryManagementSystem/Form1.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using InventoryManagementSystem.Entities;
+using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InventoryManagementSystem
@@ -14,10 +10,11 @@ namespace InventoryManagementSystem
     public partial class LoginForm : Form
     {
         private string connection = "Data Source=192.168.0.31;Initial Catalog=finalDB;Persist Security Info=True;User ID=SA;Password=password1";
-        private string sql = "INSERT INTO Users VALUES (@username,@password)";
+        private string sqlQuery;
 
         SqlConnection sqlConnection;
         SqlCommand sqlCommand;
+        User user; 
 
         public LoginForm()
         {
@@ -57,35 +54,161 @@ namespace InventoryManagementSystem
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            // username and password validation
-            if (tbUserName.Text.ToString().Length < 3 || tbPassword.Text.ToString().Length < 3)
+            login();
+            // close this form, go to next form with giving user as a parameter
+
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            signUp();
+        }
+
+        private Boolean login()
+        {
+            if (validateUser())
             {
-                MessageBox.Show("User name and password length should be longer than 3");
+                MessageBox.Show("Welcome back " + tbUserName.Text.ToString() + "!");
+                return true;
             }
             else
             {
+                MessageBox.Show("User name or password is wrong!");
+            }
+            return false;
+        }
+
+        private Boolean signUp()
+        {
+
+            if (validateInput())
+            {
                 try
                 {
-                    sqlConnection = new SqlConnection(connection); // create and open connection
-                    sqlConnection.Open();
-
-                    sqlCommand = new SqlCommand(sql, sqlConnection); // execute sql command with connection and parameters
-                    sqlCommand.Parameters.AddWithValue("username", tbUserName.Text.ToString());
-                    sqlCommand.Parameters.AddWithValue("password", tbPassword.Text.ToString());
-
-                    sqlCommand.ExecuteNonQuery();
-
-                    sqlConnection.Close();
-                    MessageBox.Show("User added");
-
+                    // if user exists
+                    if (isUserExists())
+                    {
+                        MessageBox.Show(tbUserName.Text.ToString() + " username has already taken!");
+                    }
+                    else // if user not exists create new user
+                    {
+                        if (createUser())
+                        {
+                            MessageBox.Show("User added");
+                        }
+                        else
+                        {
+                            MessageBox.Show("User could not be added");
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
                 }
             }
-            
-            // hide this form and open main form
+            return true;
+        }
+
+        private Boolean isUserExists()
+        {
+            try
+            {
+                sqlQuery = "SELECT COUNT(*) FROM Users WHERE name = '" + tbUserName.Text.ToString() + "';";
+
+                sqlConnection = new SqlConnection(connection); // create new connection and open it
+                sqlConnection.Open();
+
+                sqlCommand = new SqlCommand(sqlQuery, sqlConnection); // execute sql command with connection and parameters
+
+                // if user exists
+                if (sqlCommand.ExecuteScalar().ToString().Equals("1"))
+                {
+                    sqlConnection.Close();
+                    return true;
+                }
+
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return false;
+        }
+
+        private Boolean createUser()
+        {
+            try
+            {
+                sqlQuery = "INSERT INTO Users VALUES (@username,@password)";
+
+                sqlConnection = new SqlConnection(connection); // create new connection and open it
+                sqlConnection.Open();
+
+                sqlCommand = new SqlCommand(sqlQuery, sqlConnection); // execute sql command with connection and parameters
+                sqlCommand.Parameters.AddWithValue("username", tbUserName.Text.ToString());
+                sqlCommand.Parameters.AddWithValue("password", tbPassword.Text.ToString());
+
+                sqlCommand.ExecuteNonQuery();
+
+                sqlConnection.Close();
+                user = new User(tbUserName.Text.ToString(), tbPassword.Text.ToString());
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return false;
+        }
+
+        private Boolean validateUser()
+        {
+            if (isUserExists())
+            {
+                try
+                {
+                    sqlQuery = "SELECT name,password FROM Users WHERE name = '" + tbUserName.Text.ToString() + "';";
+
+                    sqlConnection = new SqlConnection(connection); // create new connection and open it
+                    sqlConnection.Open();
+
+                    //sqlCommand = new SqlCommand(sqlQuery, sqlConnection); // execute sql command with connection and parameters
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlQuery, sqlConnection);
+                    DataTable dataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(dataTable);
+                    DataRow rw = dataTable.AsEnumerable().FirstOrDefault(row => row.Field<String>("name") == tbUserName.Text.ToString());
+                    Console.WriteLine("LOG: " + rw.Field<string>("name") + ", " + rw.Field<string>("password"));
+
+                    if (rw.Field<string>("name").Equals(tbUserName.Text.ToString()) && rw.Field<string>("password").Equals(tbPassword.Text.ToString()))
+                    {
+                        user = new User(tbUserName.Text.ToString(), tbPassword.Text.ToString());
+                        return true;
+                    }
+                    sqlConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                // close this form and go to next form
+            }
+            return false;
+
+        }
+        private Boolean validateInput()
+        {
+            // User name and password length should be longer than 3
+            if (tbUserName.Text.ToString().Length < 3 || tbPassword.Text.ToString().Length < 3)
+            {
+                MessageBox.Show("User name and password length should be longer than 3");
+                return false;
+            }
+            return true;
         }
     }
 }
